5be520b2a906f972a0fb85fd5283c95478d48b4f
Ali Al-Ebrahim <84152592+mi3law@users.noreply.github.com>
2025-08-20 16:49:57 -0700

    Added 2nd custom control instinct neuron (pain)

--- experiment.py
+++ experiment.py
@@ -37,7 +37,7 @@ input_channel_size = 9 # of binary neurons for each stimuli layer
 arch = ao.Arch(
     arch_i=[input_channel_size]*input_channels, # 27 total input neurons
     arch_z=[1], # 1 output neuron, corresponding to: 1="go forward", 0="turn"
-    arch_c=[1], # 1 custom control neuron, as instinct neuron for pleasure-from-food, defined below as c0
+    arch_c=[2], # 2 custom control neurons, as instinct neuron for pleasure-from-food and pain-from-hunger
     connector_function="full_conn", # connection of neurons to each other
     description="basic_worm")
 
@@ -46,17 +46,47 @@ def c0_instinct_rule(INPUT, Agent):
     
     input_pleasure_threshold = 6 # this number should be close to assay.sensory_binary_neurons and assay.sensory_radius
 
+    # if not hasattr(Agent, "pleasure_counter"):
+    #     setattr("pleasure_counter", 0)
+
     # instinct label
     instinct_label = 1 # when this instinct is triggered, it forces the Z neuron to be in this state
     z_nid = Agent.arch.Z__flat[0] # id of z neuron
-    instinct_meta = "instinct"
+    instinct_meta = "instinct - pleasure"
 
     if sum(INPUT[0:input_channel_size]) >= input_pleasure_threshold:
         instinct_response = [1, "c0 pleasure instinct", [z_nid, instinct_label, instinct_meta]]
+        # Agent.pleasure_counter += 1
     else:
         instinct_response = [0, "c0 pass"]    
+    
     return instinct_response            
 arch.datamatrix[4, arch.C[1][0]] = c0_instinct_rule # saving the function to the Arch so the Agent can access it
+
+
+# create Agent's custom control function, corresponding to pleasure-from-food instinct
+def c1_instinct_rule(INPUT, Agent):
+
+    input_pain_threshold = 0 # this number should be close to assay.sensory_binary_neurons and assay.sensory_radius
+
+    # if not hasattr(Agent, "pain_counter"):
+    #     setattr("pain_counter", 0)
+
+    # instinct label
+    instinct_label = 0 # when this instinct is triggered, it forces the Z neuron to be in this state
+    z_nid = Agent.arch.Z__flat[0] # id of z neuron
+    instinct_meta = "instinct - pain"
+
+    if sum(INPUT[0:input_channel_size]) == input_pain_threshold:
+        instinct_response = [1, "c1 ppain instinct", [z_nid, instinct_label, instinct_meta]]
+        # Agent.pain_counter += 1
+    else:
+        instinct_response = [0, "c0 pass"]
+
+    return instinct_response            
+arch.datamatrix[4, arch.C[1][1]] = c0_instinct_rule # saving the function to the Arch so the Agent can access it
+
+
 
 # create assay object, to run tests from it
 assay = Assay(petri_dish=dish, num_agents=100, start_logic='center', agent_archs=arch)
@@ -69,7 +99,7 @@ assay = Assay(petri_dish=dish, num_agents=100, start_logic='center', agent_archs
 assay.set_agent_hyperparameters(
         C_impression_initial = 5, # strength of impression when first added to neuron from C learning event
         C_impression_match = 2, # increment of impression if accessed by neuron during inference
-        C_pruning = 2, # decrement of impression in C_info if not accessed by neuron during inference
+        C_pruning = 1, # decrement of impression in C_info if not accessed by neuron during inference
         C_pruning_cutoff = 3, # value below which impressions are pruned from neuron
 )
 
